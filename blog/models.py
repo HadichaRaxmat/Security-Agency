6be27dca7eb28c7da1ed10fe02a4cafe8f2233f5
HadichaRaxmat <khadicha.raxmat@gmail.com>
2025-02-01 15:58:42 +0500

    Добавлены файлы шаблонов и миграция для CustomUser

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, username=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, username=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_admin', True)
+
+        if not username:
+            raise ValueError("Superuser must have a username.")
+
+        return self.create_user(email, password, username, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField(upload_to='avatar_photos/', null=True, blank=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = CustomUserManager()
+
+    def save(self, *args, **kwargs):
+        if self.is_superuser and not self.is_staff:
+            raise ValueError("Superuser must have is_staff=True.")
+        if not self.is_active and self.is_superuser:
+            raise ValueError("Superuser cannot be deactivated.")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.email
 
 class UserContact(models.Model):
     name = models.CharField(max_length=30)
